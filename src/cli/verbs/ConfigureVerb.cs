a23e150f6aa10ed23032fd05a1a32488887c7227
Jason Goodsell <jagoodse@microsoft.com>
2019-02-19 14:36:25 +1100

    Updated Configuration Verb helper messages.

--- src/cli/verbs/ConfigureVerb.cs
+++ src/cli/verbs/ConfigureVerb.cs
@@ -5,16 +5,16 @@ namespace cli.Verbs
     [Verb("config", HelpText = "Configure the CLI")]
     public class ConfigureVerb
     {
-        [Option("azure-blob-string", Required = false, HelpText = "Set Azure Blob connection string")]
+        [Option("azure-blob-string", Required = false, HelpText = "Set Azure Blob connection string. Make sure to wrap connection string around \"\"")]
         public string AzureBlobConnectionString { get; set; }
 
         [Option('d', "default", Default = false, Required = false, HelpText = "Specify if value is default")]
         public bool IsDefault { get; set; }
 
-        [Option('k', "key", Required = false, HelpText = "They key of the setting")]
+        [Option('k', "key", Required = false, HelpText = "The key to the value in settings to change")]
         public string Key { get; set; }
 
-        [Option('v', "value", Required = false, HelpText = "The value of the setting")]
+        [Option('v', "value", Required = false, HelpText = "New value of key in settings")]
         public string Value { get; set; }
     }
 }
